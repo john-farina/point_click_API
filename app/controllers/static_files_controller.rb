bda47b06ae49c9baf2b200e20c766ca5c177692c
john-farina <clichecontent@gmail.com>
2023-03-26 22:41:54 -0400

    changed back static files

--- app/controllers/static_files_controller.rb
+++ app/controllers/static_files_controller.rb
@@ -1,21 +1,11 @@
 class StaticFilesController < ApplicationController
   def serve
-    file_path = Rails.root.join('public', params[:file_name])
-    extension = File.extname(params[:file_name])
+    file_path = File.join(Rails.public_path, params[:file_name])
 
-    case extension
-    when '.wasm'
-      content_type = 'application/wasm'
-    when '.data'
-      content_type = 'application/octet-stream'
-    when '.framework.js'
-      content_type = 'application/javascript'
-    when '.loader.js'
-      content_type = 'application/javascript'
+    if File.exist?(file_path) && !File.directory?(file_path)
+      send_file file_path, disposition: 'inline'
     else
-      raise ActionController::RoutingError.new('Not Found')
+      render file: Rails.public_path.join('404.html'), status: :not_found, layout: false
     end
-
-    send_file(file_path, filename: params[:file_name], disposition: 'inline', type: content_type)
   end
 end
