322a5c3832be8803993400af228d78f8ea1edf8f
john-farina <clichecontent@gmail.com>
2023-03-26 22:58:50 -0400

    Moved unity files prob to the incorrect place

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 pin_all_from 'app/javascript/Build', under: 'Build', to: 'Build', preload: true
 pin "@rails/actioncable", to: "actioncable.esm.js"
 pin_all_from "app/javascript/channels", under: "channels"
+pin 'app/javascript/unity', to: "unity"
